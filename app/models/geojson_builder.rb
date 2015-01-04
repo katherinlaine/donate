fdcd10f594a3c19cc70835e4e8b49d5165125879
kaffin <katherin.siracusa@gmail.com>
2015-01-04 18:13:59 -0500

    Update Maps and Search to include current data or address search

--- app/models/geojson_builder.rb
+++ app/models/geojson_builder.rb
@@ -1,17 +1,21 @@
 class GeojsonBuilder
-  def initialize(agencies)
-    @agencies = agencies
+  def initialize(locations)
+    @locations = locations
   end
 
   def to_geojson
-    @agencies.map do |agency|
-      agency_to_json(agency)
+    @locations.map do |agency|
+      agency_to_geojson(agency)
     end.to_json
+  end
+
+  def to_your_geojson_location
+    your_location_to_geojson(@locations).to_json
   end
 
   private
 
-  def agency_to_json(agency)
+  def agency_to_geojson(agency)
     {
       type: "Feature",
       geometry: {
@@ -22,6 +26,22 @@ class GeojsonBuilder
         title: agency.name,
         "marker-size" => "small",
         "marker-color" => "000000",
+        "marker-symbol" => "circle"
+      }
+    }
+  end
+
+  def your_location_to_geojson(location)
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [location.longitude, location.latitude]
+      },
+      properties: {
+        title: "Your Location",
+        "marker-size" => "small",
+        "marker-color" => "FFFFFF",
         "marker-symbol" => "circle"
       }
     }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   enum role: [:donor, :agency_admin, :super_admin]
 
   geocoded_by :ip_address,
-    latitude: :lat, longitude: :lon
+              latitude: :lat,
+              longitude: :lon
   after_validation :geocode
 
   validates :email, presence: true, uniqueness: true
